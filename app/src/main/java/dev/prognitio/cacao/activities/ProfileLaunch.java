db41f614ad78ac0820b399f6ec441d4bf2163539
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-05-22 08:25:45 -0500

    Finished PreferencesSelection (except for populating it with actual feed options) and began CourseSetup.

--- app/src/main/java/dev/prognitio/cacao/activities/ProfileLaunch.java
+++ app/src/main/java/dev/prognitio/cacao/activities/ProfileLaunch.java
@@ -33,36 +33,33 @@ public class ProfileLaunch extends AppCompatActivity {
         usernameText = findViewById(R.id.profileusernamefield);
         emailText = findViewById(R.id.profileemailfield);
 
-        proceedToPreferencesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Logger.log("ProceedToPreferences Button Activated", LogType.DEBUG, null);
+        proceedToPreferencesButton.setOnClickListener(view -> {
+            Logger.log("ProceedToPreferences Button Activated", LogType.DEBUG, null);
 
-                //retrieve and store the values in the three text fields.
+            //retrieve and store the values in the three text fields.
 
-                String nameSubmission = nameText.getText().toString();
-                String usernameSubmission = usernameText.getText().toString();
-                String emailSubmission = emailText.getText().toString();
+            String nameSubmission = nameText.getText().toString();
+            String usernameSubmission = usernameText.getText().toString();
+            String emailSubmission = emailText.getText().toString();
 
-                if (!nameSubmission.isEmpty() && !usernameSubmission.isEmpty() && !emailSubmission.isEmpty()) {
-                    //store data
-                    SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.userpref_key), Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
+            if (!nameSubmission.isEmpty() && !usernameSubmission.isEmpty() && !emailSubmission.isEmpty()) {
+                //store data
+                SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.userinfo_key), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
 
-                    editor.putString("usersupplied_name", nameSubmission);
-                    editor.putString("usersupplied_username", usernameSubmission);
-                    editor.putString("usersupplied_email", emailSubmission);
+                editor.putString("usersupplied_name", nameSubmission);
+                editor.putString("usersupplied_username", usernameSubmission);
+                editor.putString("usersupplied_email", emailSubmission);
 
-                    editor.apply();
+                editor.apply();
 
-                    //switch activity
-                    Intent switchActivityIntent = new Intent(context, PreferencesSelection.class);
-                    startActivity(switchActivityIntent);
-                } else {
-                    Logger.log("Failed to save user info in ProfileLaunch.java\nnameSubmission: " + nameSubmission
-                            + "\nusernameSubmission: " + usernameSubmission
-                            + "\nemailSubmission: " + emailSubmission, LogType.DEBUG, null);
-                }
+                //switch activity
+                Intent switchActivityIntent = new Intent(context, PreferencesSelection.class);
+                startActivity(switchActivityIntent);
+            } else {
+                Logger.log("Failed to save user info in ProfileLaunch.java\nnameSubmission: " + nameSubmission
+                        + "\nusernameSubmission: " + usernameSubmission
+                        + "\nemailSubmission: " + emailSubmission, LogType.DEBUG, null);
             }
         });
     }
